395a5f2e29b6cb7bbaf1433a856d19942b9e5a4c
ferdinan-ops <perdinantumanggor95@gmail.com>
2022-05-17 22:03:46 +0700

    update

--- admin/module/content.php
+++ admin/module/content.php
@@ -17,9 +17,8 @@ if (isset($_GET['m'])) {
 ?>
     <h1 class='mt-4''>Dashboard</h1>
     <h2 class=' fw-light'>Selamat Datang Administrator</h2>
-        <div class="bungkus d-flex justify-content-between mt-4 flex-lg-row flex-column">
-            <div class="point mt-5 mt-lg-0">
-                <h5 class="text-center pt-4">Total User</h5>
+        <div class="bungkus d-flex justify-content-between mt-5 flex-lg-row flex-column">
+            <div class="point">
                 <div class="sumUser d-flex align-items-center px-5 py-4">
                     <?php
                     $sqlUser = mysqli_query($konek, "SELECT * FROM user ");
@@ -30,7 +29,6 @@ if (isset($_GET['m'])) {
                 </div>
             </div>
             <div class="point mt-5 mt-lg-0">
-                <h5 class="text-center pt-4">Total Diskusi</h5>
                 <div class="sumUser d-flex align-items-center px-5 py-4">
                     <?php
                     $sqlDiskusi = mysqli_query($konek, "SELECT * FROM berita ");
@@ -41,7 +39,6 @@ if (isset($_GET['m'])) {
                 </div>
             </div>
             <div class="point mt-5 mt-lg-0">
-                <h5 class="text-center pt-4">Total Kategori</h5>
                 <div class="sumUser d-flex align-items-center px-5 py-4">
                     <?php
                     $sqlKategori = mysqli_query($konek, "SELECT * FROM user ");
@@ -54,24 +51,30 @@ if (isset($_GET['m'])) {
         </div>
         <div class="tabel mt-5">
             <h3 class="mb-3">Tabel Diskusi</h3>
-            <table class="table table-striped" style="width:  100%; text-align: center;">
-                <thead></thead>
+            <table class="dashboard-table" style="width:  100%; ">
                 <thead>
-                    <tr>
-                        <th scope="col">No</th>
+                    <tr class="text-center">
                         <th scope="col">Judul</th>
+                        <th scope="col" class="text-start">Kategori</th>
+                        <th scope="col">Oleh</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    $sql = mysqli_query($konek, "SELECT*FROM berita ORDER BY judul asc");
-                    $no = 1;
-                    while ($k = mysqli_fetch_array($sql)) {
-                        echo "<tr>
-                    <td>$no</td>
-                    <td>$k[judul]</td>
-                </tr>";
-                        $no++;
+                    $sqlBerita = mysqli_query($konek, "SELECT berita.*,kategori.nm_kategori,user.nama_lengkap
+                    FROM berita INNER JOIN kategori ON berita.id_kategori=kategori.id
+                    INNER JOIN user ON berita.id_user=user.id ORDER BY tgl DESC");
+                    while ($brt = mysqli_fetch_array($sqlBerita)) {
+                        echo "
+                    <tr>
+                        <td><a href='./?hal=diskusi&id=$brt[id]' class='juduld'>$brt[judul]</a></td>
+                        <td class='px-3 px-lg-0 text-center text-lg-start''>
+                            <a href=' ./?hal=diskusi&ktg=$brt[id_kategori];' class='list-group-item-success py-1 px-2 rounded-2 fs-6 link-success'>$brt[nm_kategori]
+                            </a>
+                        </td>
+                        <td><a href='./?hal=profile&id=$brt[id_user]'>$brt[nama_lengkap]</a></td>
+                    </tr>
+                    ";
                     }
                     ?>
                 </tbody>
